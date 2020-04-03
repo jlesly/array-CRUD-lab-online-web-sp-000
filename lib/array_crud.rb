24386e2b8667a2a400f5a1d82a86416956c5d6fa
Lesly Juarez <leslyj2@gmail.com>
2020-04-03 18:17:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+ create_an_empty_array = []
 end
 
 def create_an_array
-  
+  create_an_array = ["", "", "", ""]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << "element"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.shift("")
 end
 
 def remove_element_from_end_of_array(array)
